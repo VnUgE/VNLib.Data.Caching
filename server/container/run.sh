d94581d76d60b7415ab18aa433f32028db7f2cd8
vnugent <public@vaughnnugent.com>
2024-10-20 14:32:42 -0400

    update server builds and container

--- server/container/run.sh
+++ server/container/run.sh
@@ -4,12 +4,17 @@
 
 rm -rf config && mkdir config
 
-#substitude all -template files in the config-templates dir and write them to the config dir
+#substitude all -template files in the config-templates/ dir and write them to the config/ dir
+echo "Compiling server configuration"
 for file in config-templates/*-template.json; do
 	envsubst < $file > config/$(basename $file -template.json).json
 done
 
+#merge user assets with our assets dir
+echo "Merging your asset files"
 cp usr/assets/* plugins/assets/ -rf
+
+echo "Starting VNCache server"
 
 #start the server
 dotnet webserver/VNLib.WebServer.dll --config config/config.json $SERVER_ARGS
