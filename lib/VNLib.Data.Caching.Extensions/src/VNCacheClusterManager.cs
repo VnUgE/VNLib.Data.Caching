ae36eefa35b625f37486a6470588e8a0e400d614
vnugent <public@vaughnnugent.com>
2026-01-31 12:57:16 -0500

    chore: Centralize nullable and doc settings; update NoWarn, docs
    Moved Nullable and documentation file settings to Directory.Build.props for consistency. Suppressed MSTEST0001 in NoWarn. Clarified XML doc reference in VNCacheClusterManager.cs.

--- lib/VNLib.Data.Caching.Extensions/src/VNCacheClusterManager.cs
+++ lib/VNLib.Data.Caching.Extensions/src/VNCacheClusterManager.cs
@@ -124,7 +124,7 @@ namespace VNLib.Data.Caching.Extensions
         /// <exception cref="ArgumentException"></exception>
         /// <exception cref="CacheDiscoveryFailureException"></exception>
         /// <remarks>
-        /// This method simply combines the <see cref="ResolveWellKnownAsync"/> and <see cref="DiscoverNodesAsync"/>
+        /// This method simply combines the <see cref="ResolveWellKnownAsync"/> and <see cref="DiscoverNodesAsync(CancellationToken)"/>
         /// methods into a single operation
         /// </remarks>
         public async Task DiscoverNodesAsync(CancellationToken cancellation)
